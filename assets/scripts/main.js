8623ea6e3c52c9eecd54456e14155e60cda28585
LBryant01 <london.bryant.lb@gmail.com>
2024-12-04 22:29:23 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,13 @@
+// Get the dropdown menu by its ID
+const dropdown = document.getElementById('states');
 
+// Add an event listener for the 'change' event
+dropdown.addEventListener('change', function () {
+    // Get the value of the selected option
+    const selectedValue = this.value;
+
+    // If a valid value is selected, navigate to the corresponding URL
+    if (selectedValue) {
+        window.location.href = selectedValue;
+    }
+});
